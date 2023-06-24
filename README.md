0c01e749ce53ae512abb9b7d5a1aac4202396159
addetz <adelinasimion@gmail.com>
2023-06-24 16:45:40 +0100

    add e2e test and postman collection

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
 # testing-strategies-demo
+This repo contains the demo code for the talk `Comprehensive testing strategies for modern microservice architectures`. 
+
+It contains code for the conference talk server, which exposes the following endpoints: 
+```
+GET /events
+GET /events/{id}
+GET /events/{id}?day=X
+```
+For your convenience, this repo contains a Postman collection with the requests you can make to the server. See [`Conference_Talks.postman_collection.json`](./Conference_Talks.postman_collection.json).
+
+## Run tests 
+Run unit tests: 
+```
+$ go test ./... -v
+```
+
+Run integration tests:
+```
+$ INTEGRATION=true go test ./... -v
+```
+
+Run E2E tests: 
+```
+$ E2E=true go test ./... -v
+```
 
 ## Run in Docker
+The server image has been pushed to a public repo `classicaddetz/conf-talks-server`.
 
+You can run it using: 
+```
+$ $ docker run -dt -p 8000:8000/tcp classicaddetz/conf-talks-server
+```
+
+Alternatively, you can build your own and run it:
 ```
 $ docker build -f Dockerfile -t conf-talks-server .
 $ docker run -dt -p 8000:8000/tcp conf-talks-server
+```
+
+## Push image
+You can tag and push the image to your own repo as well: 
+
+```
+$ docker tag conf-talks-server username/conf-talks-server
+$ docker push username/conf-talks-server
 ```
---
+++ cmd/server/server_test.go
@@ -0,0 +1,79 @@
+package main_test
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/addetz/testing-strategies-demo/data"
+	"github.com/docker/go-connections/nat"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestServerEvents(t *testing.T) {
+	if os.Getenv("E2E") == "" {
+		t.Skip("Skipping TestServerEvents in short mode.")
+	}
+	expectedEvents := []data.Event{
+		{
+			ID:        "ewit-2023",
+			Name:      "European Women in Tech",
+			DateStart: "28/06/2023",
+			DateEnd:   "29/06/2023",
+			Location:  "Amsterdam",
+		},
+		{
+			ID:        "devbcn-2023",
+			Name:      "DevBcn - The Barcelona Developers Conference",
+			DateStart: "03/07/2023",
+			DateEnd:   "05/07/2023",
+			Location:  "Barcelona",
+		},
+		{
+			ID:        "cphdevfest-2023",
+			Name:      "Copenhagen Developers Festival",
+			DateStart: "30/08/2023",
+			DateEnd:   "01/09/2023",
+			Location:  "Copenhagen",
+		},
+	}
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "classicaddetz/conf-talks-server:latest",
+		ExposedPorts: []string{"8000/tcp"},
+		WaitingFor:   wait.ForListeningPort(nat.Port("8000")),
+	}
+	serverC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	require.Nil(t, err)
+	defer func() {
+		if err := serverC.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+	url, err := serverC.Endpoint(ctx, "http")
+	require.Nil(t, err)
+	r, err := http.Get(url + "/events")
+	require.Nil(t, err)
+	assert.Equal(t, http.StatusOK, r.StatusCode)
+
+	body, err := io.ReadAll(r.Body)
+	r.Body.Close()
+	require.Nil(t, err)
+
+	var resp data.Events
+	err = json.Unmarshal(body, &resp)
+	require.Nil(t, err)
+	assert.Len(t, resp.Events, len(expectedEvents))
+	for _, e := range expectedEvents {
+		assert.Contains(t, resp.Events, e)
+	}
+}
